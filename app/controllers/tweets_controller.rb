d004a64ab016b7938d24c65398624871a5900f5c
ssshhhiiinnn <shin.3534.a@gmail.com>
2017-10-09 15:55:26 +0900

    完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,5 +5,15 @@ class TweetsController < ApplicationController
 
   def new
   end
-    
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:text)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/tweets' => 'tweets#index'
   get '/tweets/new' => 'tweets#new'
+  post '/tweets/created' => 'tweets#create'
 
 end
